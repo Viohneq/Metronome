9e3208e02b4fffad683ff8c7f755f92052997760
Yuriy Egorov <yuriy.egorov@team.bumble.com>
2023-07-22 15:31:45 +0400

    Added first instance of the project

---
+++ Metronome/Metronome/MetronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetronomeApp.swift
+//  Metronome
+//
+//  Created by Yuriy Egorov on 22/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MetronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
